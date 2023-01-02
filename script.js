c277907aa56d0d0bf8a207969f3a854b96c3cdfc
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-02 22:34:46 +0330

    add audio for Piano

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const note = document.querySelector('.nowplaying');
 const keys = document.querySelectorAll('.key');
+const hints = document.querySelectorAll('.hints')
 
 window.addEventListener('keydown' , function(e) {
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
- 
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+
     if(! key) return;
 
     const keyNote = key.getAttribute('data-note');
  
     key.classList.add('playing');
     note.innerHTML = keyNote;
-
+    audio.currenttime = 0;
+    audio.play();
 
 });
 
@@ -19,3 +22,7 @@ function removeTransition() {
 }
 
 keys.forEach(key => key.addEventListener('transitionend' , removeTransition))
+
+hints.forEach(function(elm , index){
+    elm.style = `transition-delay : ${index*50}ms`
+})
